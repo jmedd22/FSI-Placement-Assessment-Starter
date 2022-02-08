3b7302b6650e445390393ef9b3c1d8a65f50c759
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-07 20:09:44 -0600

    added function to gb buttons

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Justin Meddaugh" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
+
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
@@ -11,17 +12,44 @@ let sugar = 0 // Sugar Sprinkle
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+const gbAddBtn = document.querySelector('#add-gb')
+let gbMinusBtn = document.querySelector('#minus-gb')
+let gbQty = document.querySelector('#qty-gb')
+let ccQty = document.querySelector('#qty-cc')
+let sugarQty = document.querySelector('#qty-sugar')
+let cookieTotal = document.querySelector('#qty-total')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+gbAddBtn.addEventListener('click', function(e) {
+    gb++
+    gbQty.textContent=`${gb}`
+})    
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbMinusBtn.addEventListener('click',function(e) {
+    gb--
+    gbQty.textContent=`${gb}`
 })
 
+
+
+
+
+
+
+
+
+
+// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+
 // TODO: Hook up event listeners for the rest of the buttons
+
+
+// let gbAddBtn = document.querySelector('#add-gb')
+// let gbMinusBtn = document.querySelector('#minus-gb')
+// let ccAddBtn = document.querySelector('#add-cc')
+// let ccMinusBtn = document.querySelector('#minus-cc')
+// let sugarAddBtn = document.querySelector('#add-sugar')
+// let sugarMinusBtn = document.querySelector('#minus-sugar')
